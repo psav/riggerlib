b3b74714f9cc82963e5cdc558aa408f68fe0ad98
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-07-28 09:26:35 +0200

    add forgotten api entrypoint and add test for api entrypoints

--- riggerlib/__init__.py
+++ riggerlib/__init__.py
@@ -3,5 +3,6 @@ from __future__ import print_function
 from .client import RiggerClient
 from .server import Rigger
 from .plugins import RiggerBasePlugin
+from .tools import recursive_update
 
-__all__ = ["Rigger", "RiggerClient", 'RiggerBasePlugin']
+__all__ = ["Rigger", "RiggerClient", 'RiggerBasePlugin', 'recursive_update']
